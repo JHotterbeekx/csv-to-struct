8b21731846776ef04e87343316facf6d1754a82f
John Hotterbeekx <john.online@hotterbeekx.nl>
2017-08-02 23:36:35 +0200

    First version, works a little

---
+++ CsvReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace CsvReader
+{
+  public class CsvReader
+  {
+
+    public List<T> Read<T>(string filename) where T : struct
+    {
+      var splitter = new StringSplitter(6);
+      var result = new List<T>();
+
+      using (StreamReader sr = File.OpenText(filename))
+      {
+        var headers = splitter.Split(sr.ReadLine(), ',');
+        var genericType = typeof (T);
+        FieldInfo[] propertyIndex = new FieldInfo[6];
+        for (int i = 0; i < headers.Length; i++)
+        {
+          propertyIndex[i] = genericType.GetField(headers[i]);
+        }
+
+        string s = String.Empty;
+        string[] data;
+        FieldInfo activeProp;
+        
+        while ((s = sr.ReadLine()) != null)
+        {
+          ValueType record = new T();
+          //we're just testing read speeds
+          data = splitter.Split(s, ',');
+          for (int i = 0; i < headers.Length; i++)
+          {
+            activeProp = propertyIndex[i];
+            activeProp.SetValue(record, _Convert(data[i], activeProp.FieldType));
+          }
+          result.Add((T)record);
+        }
+      }
+      return result;
+    }
+
+    private object _Convert(string value, Type type)
+    {
+      if (type == typeof (string)) return value;
+      if (type == typeof (int)) return CustomParseInt(value);
+      return null;
+    }
+
+    private int res = 0;
+    private int CustomParseInt(string s)
+    {
+      var res = 0;
+      foreach (var c in s) {
+        res = 10 * res + (c - '0');
+      }
+      return res;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReader {
+  class Program {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var data = new CsvReader().Read<testData>(@"C:/temp/MOCK_DATA.csv");
+        data = null;
+
+        sw.Stop();
+        Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+      }
+      Console.ReadLine();
+    }
+  }
+
+  struct testData
+  {
+    public int id;
+    public string first_name;
+    public string last_name;
+    public string email;
+    public string gender;
+    public string ip_address;
+  }
+}
